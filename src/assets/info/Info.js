957e25d2695e7c9e153475659873d1351b1be83f
sanjay <sanjaynainwal129@gmail.com>
2025-02-11 13:20:10 +0530

    updated resume

--- src/assets/info/Info.js
+++ src/assets/info/Info.js
@@ -35,7 +35,7 @@ export const info = {
   ],
   socials: [
     {
-      link: 'https://drive.google.com/file/d/1DFgiZABKJtT2RbzHSuf8bgFG8QIZeKyf/view?usp=sharing', // this should be https://yourname.com/resume.pdf once you've deployed
+      link: 'https://drive.google.com/file/d/1ofgDpI3sLCu6xC54Btn6CjFamFuDneh6/view?usp=sharing', // this should be https://yourname.com/resume.pdf once you've deployed
       icon: 'fa fa-file-pdf-o',
       label: 'resume',
     },
